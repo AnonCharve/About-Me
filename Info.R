30cb8dc95e7a2f50d1dea2cec1f4ea70677032d1
AnonCharve <carlosvelonza@gmail.com>
2022-06-16 15:21:29 +0800

    About Me

--- Info.R
+++ Info.R
@@ -1,3 +1,4 @@
 print("My name is John Carlo Velonza")
 print("You can call me Carlo")
 print("One of my past time activities is hangging out with friends over a PC game")
+print("I chose to be in EcE because I was interested in being like my parent who are also EcE Engineers")
